74c75b46684ceef23e99ea950da4027604438e47
stephendawsondev <>
2023-05-01 10:22:23 +0000

    add code to increment incorrect or correct score

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,8 +50,10 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if (isCorrect) {
+        incrementScore();
         alert(`Hey! You got it right! :D`);
     } else {
+        incrementWrongAnswer();
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
     }
 
@@ -76,12 +78,22 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the
+ * DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
+/**
+ * Gets the current score from the
+ * DOM and decrements it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
